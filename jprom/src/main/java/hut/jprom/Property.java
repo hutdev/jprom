10c060165e89d5418dab8fc100c4902d86578ac5
hutdev <hutdevelopment@gmail.com>
2015-12-16 21:29:14 +0100

    Property annotations

---
+++ jprom/src/main/java/hut/jprom/Property.java
@@ -0,0 +1,39 @@
+/*
+  * Copyright (c) 2015, hutdev <hutdevelopment@gmail.com>
+  * Permission to use, copy, modify, and/or distribute this software for any
+  * purpose with or without fee is hereby granted, provided that the above
+  * copyright notice and this permission notice appear in all copies.
+  * 
+  * THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES WITH
+  * REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF MERCHANTABILITY
+  * AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY SPECIAL, DIRECT,
+  * INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES WHATSOEVER RESULTING FROM
+  * LOSS OF USE, DATA OR PROFITS, WHETHER IN AN ACTION OF CONTRACT, NEGLIGENCE OR
+  * OTHER TORTIOUS ACTION, ARISING OUT OF OR IN CONNECTION WITH THE USE OR
+  * PERFORMANCE OF THIS SOFTWARE.
+ */
+package hut.jprom;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+/**
+ * Marks a field which will be represented in a properties file.
+ *
+ * @author hutdev <hutdevelopment@gmail.com>
+ */
+@Target(ElementType.FIELD)
+@Retention(RetentionPolicy.RUNTIME)
+public @interface Property {
+
+    /**
+     * Optional name of the field as it will be represented in a properties
+     * file. If left to default, the declared name of the annotated field will
+     * be used.
+     *
+     * @return The property name of the field.
+     */
+    String name() default "";
+}
